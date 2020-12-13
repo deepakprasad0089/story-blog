c46f1c316421c243e90a900b6b2bf0b4660a2e4a
deelpixel <52972857+deelpixel@users.noreply.github.com>
2020-12-13 18:33:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template,request
 app=Flask(__name__)
+entries=[]
 
 @app.route("/home",methods=["GET","POST"])
 def home():
    if request.method=="POST":
        entry_content=request.form.get("content")
-   else:
-       return render_template("home.html")
+       entries.append(entry_content)
+       #return render_template("home.html")
+   #else:
+   return render_template("home.html")
